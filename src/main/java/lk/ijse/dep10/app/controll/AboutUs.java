f1b8ac131c97656eaa0b1498cd2357aa4bd8972a
Subhash-Chamara <udayanga.subash@gmail.com>
2023-02-26 17:40:00 +0530

    View is Designed

---
+++ src/main/java/lk/ijse/dep10/app/controll/AboutUs.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep10.app.controll;
+
+public class AboutUs {
+}
---
+++ src/main/java/lk/ijse/dep10/app/controll/EditorView.java
@@ -0,0 +1,139 @@
+package lk.ijse.dep10.app.controll;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+
+public class EditorView {
+
+    @FXML
+    private Button btnDown;
+
+    @FXML
+    private Button btnReplace;
+
+    @FXML
+    private Button btnReplaceAll;
+
+    @FXML
+    private Button btnUp;
+
+    @FXML
+    private CheckBox chkMatchCase;
+
+    @FXML
+    private Label lblResult;
+
+    @FXML
+    private MenuItem mnAbout;
+
+    @FXML
+    private MenuItem mnClose;
+
+    @FXML
+    private MenuItem mnNew;
+
+    @FXML
+    private MenuItem mnOpen;
+
+    @FXML
+    private MenuItem mnPrint;
+
+    @FXML
+    private MenuItem mnSave;
+
+    @FXML
+    private MenuItem mnSaveAS;
+
+    @FXML
+    private TextArea txtEditor;
+
+    @FXML
+    private TextField txtFind;
+
+    @FXML
+    private TextField txtReplace;
+
+    @FXML
+    void btnDownOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReplaceAllOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReplaceOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chkMatchCaseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnAboutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnCloseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnOpenOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnPrintOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnSaveAsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rootOnDragDropped(DragEvent event) {
+
+    }
+
+    @FXML
+    void rootOnDragOver(DragEvent event) {
+
+    }
+
+    @FXML
+    void txtEditorOnMouseClicked(MouseEvent event) {
+
+    }
+
+}
